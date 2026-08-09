1c3fda43fe116368eea4f4a59b7834a7aa2ade77
Awaab Ali Arshad <awaabali.works@gmail.com>
2026-08-09 12:03:53 +0500

    Squad Feature Test

--- src/main/java/com/projects/Football/Club/Management/System/service/SquadService.java
+++ src/main/java/com/projects/Football/Club/Management/System/service/SquadService.java
@@ -6,7 +6,9 @@ import com.projects.Football.Club.Management.System.entity.SquadRole;
 import com.projects.Football.Club.Management.System.repository.SquadEntryRepo;
 import com.projects.Football.Club.Management.System.repository.SquadRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class SquadService {
 
     @Autowired
@@ -15,34 +17,8 @@ public class SquadService {
     @Autowired
     SquadEntryRepo squadEntryRepo;
 
-    public boolean checkTeamMemberShip(SquadEntry squadEntry){
-        return squadEntry.getPlayer().getTeam().equals(squadEntry.getSquad().getTeam());
-    }
 
-    public void saveSquadEntry(SquadEntry squadEntry){
-        squadEntryRepo.save(squadEntry);
-        squadRepo.save(squadEntry.getSquad());
-    }
-    public boolean squadSizeCheck(SquadEntry squadEntry){
-        return squadEntry.getSquad().getSquadEntries().size() < 20;
-    }
-    public boolean checkStarterPlayersLimit(SquadEntry squadEntry){
-        int max = 0;
-        for (int i = 0; i < squadEntry.getSquad().getSquadEntries().size(); i++) {
-            if (squadEntry.getSquad().getSquadEntries().get(i).getRole().equals(SquadRole.STARTER))
-                max++;
-        }
-        return max < 11;
-    }
-    private boolean checkSubPlayersLimit(SquadEntry squadEntry) {
-        int max = 0;
-        for (int i = 0; i < squadEntry.getSquad().getSquadEntries().size(); i++) {
-            if (squadEntry.getSquad().getSquadEntries().get(i).getRole().equals(SquadRole.SUBSTITUTE))
-                max++;
-        }
-        return max < 9;
-    }
-
+    // controller linked methods
     public void createSquad() {
         squadRepo.save(new Squad());
     }
@@ -56,9 +32,10 @@ public class SquadService {
             return;
         if (checkStarterPlayersLimit(squadEntry))
             return;
-        if(checkSubPlayersLimit(squadEntry))
-        squadEntry.getSquad().getSquadEntries().add(squadEntry);
-        saveSquadEntry(squadEntry);
+        if (checkSubPlayersLimit(squadEntry)) {
+            squadEntry.getSquad().getSquadEntries().add(squadEntry);
+            saveSquadEntry(squadEntry);
+        }
 
 
     }
@@ -66,8 +43,8 @@ public class SquadService {
 
     public void removePlayer(int squadEntryId) {
         SquadEntry squadEntry = squadEntryRepo.findById(squadEntryId).orElseThrow();
-        for(SquadEntry s:squadEntry.getSquad().getSquadEntries()){
-            if(s.getSquad().equals(squadEntry.getSquad())){
+        for (SquadEntry s : squadEntry.getSquad().getSquadEntries()) {
+            if (s.getSquad().equals(squadEntry.getSquad())) {
                 squadEntry.getSquad().getSquadEntries().remove(squadEntry);
                 saveSquadEntry(squadEntry);
             }
@@ -81,15 +58,47 @@ public class SquadService {
         SquadEntry squadEntry2 = squadEntryRepo.findById(squadEntryId2).orElseThrow();
 
         //check if in the same squad
-        if(!squadEntry1.getSquad().equals(squadEntry2.getSquad()))
+        if (!squadEntry1.getSquad().equals(squadEntry2.getSquad()))
             return;
         //check if players have different roles
-        if(squadEntry1.getRole().equals(squadEntry2.getRole()))
+        if (squadEntry1.getRole().equals(squadEntry2.getRole()))
             return;
         SquadRole squadRole = squadEntry1.getRole();
         squadEntry1.setRole(squadEntry2.getRole());
         squadEntry2.setRole(squadRole);
         saveSquadEntry(squadEntry1);
         saveSquadEntry(squadEntry2);
+    }
+
+    //methods for reusability
+    public boolean checkTeamMemberShip(SquadEntry squadEntry) {
+        return squadEntry.getPlayer().getTeam().equals(squadEntry.getSquad().getTeam());
+    }
+
+    public void saveSquadEntry(SquadEntry squadEntry) {
+        squadEntryRepo.save(squadEntry);
+        squadRepo.save(squadEntry.getSquad());
+    }
+
+    public boolean squadSizeCheck(SquadEntry squadEntry) {
+        return squadEntry.getSquad().getSquadEntries().size() < 20;
+    }
+
+    public boolean checkStarterPlayersLimit(SquadEntry squadEntry) {
+        int max = 0;
+        for (int i = 0; i < squadEntry.getSquad().getSquadEntries().size(); i++) {
+            if (squadEntry.getSquad().getSquadEntries().get(i).getRole().equals(SquadRole.STARTER))
+                max++;
+        }
+        return max < 11;
+    }
+
+    private boolean checkSubPlayersLimit(SquadEntry squadEntry) {
+        int max = 0;
+        for (int i = 0; i < squadEntry.getSquad().getSquadEntries().size(); i++) {
+            if (squadEntry.getSquad().getSquadEntries().get(i).getRole().equals(SquadRole.SUBSTITUTE))
+                max++;
+        }
+        return max < 9;
     }
 }
