6c451edec157e0a2711f61c07b202bd22a072166
Awaab Ali Arshad <awaabali.works@gmail.com>
2026-08-13 05:32:20 +0500

    Exception handling started

---
+++ src/main/java/com/projects/Football/Club/Management/System/exception/GlobalExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.projects.Football.Club.Management.System.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+}
---
+++ src/main/java/com/projects/Football/Club/Management/System/exception/ResourceNotFound.java
@@ -0,0 +1,7 @@
+package com.projects.Football.Club.Management.System.exception;
+
+public class ResourceNotFound extends RuntimeException {
+    public ResourceNotFound(String message) {
+        super(message);
+    }
+}
